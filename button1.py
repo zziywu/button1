a529ff15d2e1fc5dbb94e4efc5cc5caeae491a78
Jiyu <yvmer077@mju.ac.kr>
2026-09-16 10:20:35 +0900

    modify name and hakbun

--- button1.py
+++ button1.py
@@ -2,8 +2,8 @@ from tkinter import *
 
 window =Tk()
 
-b1 = Button(window, text="Yeonseung Ryu")
-b2 = Button(window, text="60251111")
+b1 = Button(window, text="Jiyu Bang")
+b2 = Button(window, text="60251814")
 b1.grid(row=0, column=0)
 b2.grid(row=1, column=1)
 
